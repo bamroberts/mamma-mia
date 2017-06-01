4cb3aea93e2db185b04e2aa47b7cc3a258f3955a
bamroberts <iam@bamroberts.com>
2017-06-01 19:30:52 +1200

    Auditions on CMS

---
+++ mysite/code/CMSAuditions.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class CMSAuditions extends LeftAndMain implements PermissionProvider{
+
+	private static $url_segment = 'auditions';
+
+	private static $url_rule = '/$Action/$ID';
+
+	private static $menu_title = 'Auditions';
+
+	private static $tree_class = 'Folder';
+
+	public function Auditions() {
+		return Audition::get()->sort('Date ASC, Time Asc');
+	}
+
+	public function getEditForm($id = null, $fields = null) {
+		
+			// List all reports
+			$fields = new FieldList();
+			$gridFieldConfig = GridFieldConfig::create()->addComponents(
+				new GridFieldToolbarHeader(),
+				new GridFieldFilterHeader(),
+				new GridFieldSortableHeader(),
+				new GridFieldDataColumns(),
+				new GridFieldFooter(),
+				new GridFieldExportButton()
+
+			);
+			$gridField = new GridField('Auditions',false, $this->Auditions(), $gridFieldConfig);
+			$columns = $gridField->getConfig()->getComponentByType('GridFieldDataColumns');
+			$columns->setDisplayFields(array(
+				'Date' => 'Date',
+				'Time' => 'Time',
+				'FirstName' => 'First Name',
+				'Surname' => 'Surname',
+				'Email' => 'Email',
+				'Role' => 'Role',
+				'DanceWorkshop1' => 'Dance Workshop 1',
+				'DanceWorkshop2' => 'Dance Workshop 2',
+				'Comment' => 'Comment'
+			));
+			$columns->setFieldCasting(array(
+				'Date' => 'Date->Nice',
+			));
+			$columns->setFieldFormatting(array(
+				'Comment' => function($value, $item) {
+					$comment = $value;
+					if($item->Reason) {
+						$comment .= "Reason for Saturday Audition: " . $item->Reason;
+					}
+					return $comment;
+				},
+			));
+
+			$export = $gridField->getConfig()->getComponentByType('GridFieldExportButton');
+
+			
+			$fields->push($gridField);
+		
+
+		$actions = new FieldList();
+		$form = new Form($this, "EditForm", $fields, $actions);
+		$form->addExtraClass('cms-edit-form cms-panel-padded center ' . $this->BaseCSSClasses());
+		$form->loadDataFrom($this->request->getVars());
+
+		$this->extend('updateEditForm', $form);
+
+		return $form;
+	}
+}
